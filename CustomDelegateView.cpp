ed2a3d02979c5af19bf6b240ee06ad4374d3ab0d
ArtemK <kuznetsov-95-95@mail.ru>
2023-07-25 16:49:38 +0300

    Ячейка с кнопкой в таблице переделана через делегат

---
+++ CustomDelegateView.cpp
@@ -0,0 +1,31 @@
+#include "CustomDelegateView.h"
+#include <QApplication>
+#include <QDebug>
+
+CustomDelegateView::CustomDelegateView(QObject *parent) :
+    QItemDelegate(parent)
+{
+
+}
+
+void CustomDelegateView::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
+{
+    QItemDelegate::paint(painter, option, index);
+    if(index.column() == 3) {
+        QStyleOptionButton button;
+        button.rect = QRect(option.rect.left()+1, option.rect.top()+1, option.rect.width() - 2, option.rect.height() - 2);
+        button.features = QStyleOptionButton::None;
+        button.text = "Удaлить точку";
+        button.state = QStyle::State_Enabled;
+        QApplication::style()->drawControl(QStyle::CE_PushButton, &button, painter);
+    }
+}
+
+bool CustomDelegateView::editorEvent(QEvent *event, QAbstractItemModel *model, const QStyleOptionViewItem &option, const QModelIndex &index)
+{
+    QItemDelegate::editorEvent(event, model, option,index);
+    if(event->type() == QEvent::MouseButtonRelease) {       //При нажатии испускать сигнал
+        emit signalClicked(index);
+    }
+    return true;
+}
